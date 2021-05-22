e17d75767193e5af0098979c8ee9d543849521fd
aishivam <shivamhande0000@gmail.com>
2021-05-23 00:27:30 +0530

    cwides

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./Components/header.component";
+import Nav from "./Components/nav.component";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav/>
+      <Header/>
+
+      <hr></hr>
+      <p class='cop' >
+            Copyright &#169;2017-2021 Some rights reserved | <a href='https://www.cwipedia.in' >cwipedia.in</a>
+</p>
     </div>
   );
 }
